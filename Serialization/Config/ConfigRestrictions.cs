0c519acaca4d4e13661dfcaa400702d8801614d8
Akshin Mustafayev <akshin1995@outlook.com>
2021-10-13 20:46:49 +0400

    EasyJob 1.1.1

--- Serialization/Config/ConfigRestrictions.cs
+++ Serialization/Config/ConfigRestrictions.cs
@@ -12,6 +12,21 @@ namespace EasyJob.Serialization
         public bool block_buttons_remove { get; set; }
         public bool block_tabs_add { get; set; }
         public bool block_buttons_add { get; set; }
+        public bool block_buttons_reorder { get; set; }
         public bool block_buttons_edit { get; set; }
+        public bool hide_menu_item_file_reload_config { get; set; }
+        public bool hide_menu_item_file_open_app_folder { get; set; }
+        public bool hide_menu_item_file_clear_events_list { get; set; }
+        public bool hide_menu_item_settings { get; set; }
+        public bool hide_menu_item_settings_workflow { get; set; }
+        public bool hide_menu_item_settings_workflow_reorder_tabs { get; set; }
+        public bool hide_menu_item_settings_workflow_add_tab { get; set; }
+        public bool hide_menu_item_settings_workflow_remove_current_tab { get; set; }
+        public bool hide_menu_item_settings_workflow_rename_current_tab { get; set; }
+        public bool hide_menu_item_settings_workflow_add_button_to_current_tab { get; set; }
+        public bool hide_menu_item_settings_workflow_reorder_buttons_in_current_tab { get; set; }
+        public bool hide_menu_item_help { get; set; }
+        public bool hide_menu_item_help_troubleshooting { get; set; }
+        public bool hide_menu_item_help_about { get; set; }
     }
 }
