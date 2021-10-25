a4e3b94af79c48d4d40cbd83f6064c5ecc5327f0
Akshin Mustafayev <akshin1995@outlook.com>
2021-10-25 20:57:45 +0400

    EasyJob 1.1.5

---
+++ EasyJobPSTools/Program.cs
@@ -0,0 +1,70 @@
+﻿using EasyJobPSTools.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyJobPSTools
+{
+    public class Program
+    {
+        public static string ShowEJInputBoxWindow(string Header, string Text, bool AllowEmptyResult)
+        {
+            ShowEJInputBox sejib = new ShowEJInputBox(Header, Text, AllowEmptyResult);
+            if (sejib.ShowDialog() == true)
+            {
+                return sejib.windowResult;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static string ShowEJSelectFileWindow()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static string ShowEJSelectFileWindow(string fileType)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = fileType;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static string ShowEJSelectFolderWindow()
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    return fbd.SelectedPath;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+    }
+}
